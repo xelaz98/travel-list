fad48af89bb940f00240a8acf41675cda01c94c1
AlexP <68079031+xelaz98@users.noreply.github.com>
2025-01-27 22:43:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
